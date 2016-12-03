62706c813b38992c0e9ecd169f1d43777fdb969a
jpdef <jacobdefilippis@gmail.com>
2016-12-03 14:38:23 -0800

    new dirs for structure and for realtime platform

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 .PHONY: depend clean print
-CC        =  arm-linux-gnueabihf-g++
+#CC        =  arm-linux-gnueabihf-g++
+CC         =  ../tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-g++
+
 CFLAGS    = -Wall -std=c++11 
-LIBS      = -lm
+LIBS      = -lm -lrt
 SRCS      = $(wildcard src/*.cpp)
 HDRS      = $(patsubst src/%.cpp,src/%.h,$(SRCS))
 OBJS      = $(patsubst src/%.cpp,build/%.o,$(SRCS))
@@ -10,7 +12,6 @@ SRCDIR    = src/
 BUILDDIR  = build/
 BINDIR    = bin/
 all : $(BIN) 
-
 
 $(BIN) : $(OBJS) 
 	$(CC) -o  $(BINDIR)$(BIN)  $^ $(LIBS)
---
+++ src/artifacts/idm.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+#include "bcm2835.h"
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+int main(int argc, char **argv) {
+  printf("MPU6050 3-axis acceleromter example program\n");
+  I2Cdev::initialize();
+  MPU6050 accelgyro ;
+  accelgyro.resetGyroscopePath()
+  accelgyro.resetAccelerometerPath()
+  accelgyro.resetTemperaturePath()
+  accelgyro.resetI2CMaster()
+//  int16_t ax, ay, az;
+//  int16_t gx, gy, gz;
+//  if ( accelgyro.testConnection() ) 
+//    printf("MPU6050 connection test successful\n") ;
+//  else {
+//    fprintf( stderr, "MPU6050 connection test failed! something maybe wrong, continuing anyway though ...\n");
+//    return 1;
+//  }
+//  accelgyro.initialize();
+//  printf("\n");
+//  printf("  ax \t ay \t az \t gx \t gy \t gz:\n");
+////  while (true) {
+//    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+//printf("  %d \t %d \t %d \t %d \t %d \t %d\r", ax, ay, az, gx, gy, gz);
+////fflush(stdout);
+////    bcm2835_delay(100);
+////  }
+//  return 1; 
+}
